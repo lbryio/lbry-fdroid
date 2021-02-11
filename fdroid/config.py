80e48be49c7e5db3428125e01adfe49c0e962ee6
iamflorencejay <24932514+iamflorencejay@users.noreply.github.com>
2021-02-11 21:03:02 +0800

    building upstream repo

---
+++ fdroid/config.py
@@ -0,0 +1,8 @@
+
+repo_keyalias = "localhost"
+
+keydname = "CN=localhost, OU=F-Droid"
+
+keystorepass = "EBaPlKDRGJ7lGUDARyRFyTwGGvy6CbVAfNElEAF9/ko="
+
+keypass = "EBaPlKDRGJ7lGUDARyRFyTwGGvy6CbVAfNElEAF9/ko="
